de5a83d350843093f7394f4210135c385fe40752
Konstantinos Perifanos <kperifanos2@gmail.com>
2024-05-18 22:00:45 +0300

    Analytics page initialized and dockerized submit_ms

--- frontend/app/analytics/page.js
+++ frontend/app/analytics/page.js
@@ -1,10 +1,63 @@
 import React from 'react'
+import '@/Styles/analytics.css'
+
+const solvers = [{
+  problemsSubmitted: 2,
+  problemsSolved: 2,
+  AverageTimeAfterSubmission: 1,
+  AverageTimeAfterSubmissionHR: 1,
+  MinimumTimeAfterSubmission: 1,
+  MinimumTimeAfterSubmissionHR: 1,
+  MaximumTimeAfterSubmission: 1,
+  MaximumTimeAfterSubmissionHR: 1,
+}]
 
 const page = () => {
   return (
-    <div>
-        
-    </div>
+    <main className='analytics_container'>
+      <section className='total_stats_container'>
+        <p>Number of problems: </p>
+        <p>Number of finished problems: </p>
+        <p>Average Time after submission: </p>
+        <p>Average Time after submission HR: </p>
+        <p>Minimum time after submission: </p>
+        <p>Minimum time after submission HR: </p>
+        <p>Maximum time after submission: </p>
+        <p>Maximum time after submission HR: </p>
+      </section>
+      <section className='solver_stats_container'>
+        <h2>Solver stats: </h2>
+        <ul>
+          {
+            solvers.map((item) => {
+              return(
+                <li>
+                  <p>Problems Submitted: {item.problemsSolved}</p>
+                  <p>Problems solved: {item.problemsSolved}</p>
+                  <p>Average Time after submission: {item.AverageTimeAfterSubmission}</p>
+                  <p>Average Time after submission HR: {item.AverageTimeAfterSubmissionHR}</p>
+                  <p>Minimum time after submission: {item.MinimumTimeAfterSubmission} </p>
+                  <p>Minimum time after submission HR: {item.MinimumTimeAfterSubmissionHR}</p>
+                  <p>Maximum time after submission: {item.MaximumTimeAfterSubmission}</p>
+                  <p>Maximum time after submission HR: {item.MaximumTimeAfterSubmissionHR}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+      <section className='monthly_stats_container'>
+        <h2>Monthly Stats</h2>
+        <p>Month: </p>
+        <p>Number of finished problems: </p>
+        <p>Average Time after submission: </p>
+        <p>Average Time after submission HR: </p>
+        <p>Minimum time after submission: </p>
+        <p>Minimum time after submission HR: </p>
+        <p>Maximum time after submission: </p>
+        <p>Maximum time after submission HR: </p>
+      </section>
+    </main>
   )
 }
 
--- submit_ms/kafka/kafka.js
+++ submit_ms/kafka/kafka.js
@@ -4,7 +4,7 @@ const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
     clientID: 'problem_handle',
-    brokers: ['localhost:9092'], // assuming Kafka is running locally on the default port
+    brokers: [`${process.env.KAFKA_BROKER}`], // assuming Kafka is running locally on the default port
     ssl: false // assuming SSL is not enabled in your local setup
 });
 
