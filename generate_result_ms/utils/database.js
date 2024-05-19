a04218a20a03d4f7a23366f2cc8c7cc524955477
Manos <manos1939@gmail.com>
2024-05-19 18:53:22 +0300

    initialize sqlite on docker build for generate-result

--- generate_result_ms/utils/database.js
+++ generate_result_ms/utils/database.js
@@ -1,7 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Connect to SQLite database
-const db = new sqlite3.Database('../data/mydatabase.db');
+const db = new sqlite3.Database('../data/mydatabase.db',  sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
+
+});
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS Solver (
