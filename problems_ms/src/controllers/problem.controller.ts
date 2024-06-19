f7327d1c2499d71b9aa3aa9312e9ff17c4d61397
Manos <manos1939@gmail.com>
2024-06-19 13:19:44 +0300

    problem ms http status codes fix

--- problems_ms/src/controllers/problem.controller.ts
+++ problems_ms/src/controllers/problem.controller.ts
@@ -9,7 +9,7 @@ export const getAllProblems = async (req: Request, res: Response) => {
     // get all problems
     const problems = await database.getRepository(Problem).find({relations: ['datasets', 'metadata']});
     if(problems.length === 0) {
-        res.status(404).json({ message: 'No problems found!' });
+        res.status(200).json({ message: 'No problems found!' });
         return;
     }
     res.json(problems);
@@ -23,7 +23,7 @@ export const getProblem = async (req: Request, res: Response) => {
         relations: ['datasets', 'metadata']
     });
     if (!problem) {
-        res.status(404).json({ message: 'Problem not found!' });
+        res.status(400).json({ message: 'Problem not found!' });
         return;
     }
     res.json(problem);
@@ -37,7 +37,7 @@ export const deleteProblem = async (req: Request, res: Response) => {
         relations: ['datasets', 'metadata']
     });
     if (!problem) {
-        res.status(404).json({ message: 'Problem not found!' });
+        res.status(400).json({ message: 'Problem not found!' });
         return;
     }
     await database.getRepository(Dataset).delete({ problem: { id: problemId } });
